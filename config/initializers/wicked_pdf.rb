89c66f6d63441f30b28b889093d45ce6b213e357
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-02-18 18:15:33 -0700

    commit

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,7 @@
-if Rails.env.production?
-  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
-else
-  wkhtmltopdf_path = '/usr/local/bin/wkhtmltopdf'
-end
+# if Rails.env.production?
+#   wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+# else
+#   wkhtmltopdf_path = '/usr/local/bin/wkhtmltopdf'
+# end
 
-WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+# WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
