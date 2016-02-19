b5e7caf83eb4f353ae1c32a43698ab28402d139e
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-02-18 18:33:17 -0700

    autho specific emails

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,1 @@
-# if Rails.env.production?
-#   wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
-# else
-#   wkhtmltopdf_path = '/usr/local/bin/wkhtmltopdf'
-# end
-
-# WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+#
